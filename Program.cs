09420ad9fc3206eb97501d2d89eef28ad95d9769
SYARGEY <lss1895@mail.ru>
2023-03-08 13:18:17 +0300

    добавлен файл README.md

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
 ﻿// 
-void PrintArray(int[] array)
+void PrintArray(string[] array)
 {
-    foreach (int item in array)
+    foreach (string item in array)
     {
         System.Console.Write($"{item} ");
     }
 }
+string[] array = { "hellow", "2", ":-)" };
+string[] newArray= new string[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[i] = array[i];
+    }
+}
+    System.Console.WriteLine("Текущий массив");
+    PrintArray(array);
+    System.Console.WriteLine();
+    System.Console.WriteLine("Новый массив");
+    PrintArray(newArray);
+
+
 
